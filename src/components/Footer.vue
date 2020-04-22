1d4c92cc3404492f6e3158be184601bf8ee11810
Jorge Martín <grusite@gmail.com>
2020-04-22 13:55:35 +0200

    added Parallax vuetify template & created footer comp

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-footer
+      class="justify-center"
+      color="#292929"
+      height="100"
+    >
+      <div class="title font-weight-light grey--text text--lighten-1 text-center">
+        &copy; {{ (new Date()).getFullYear() }} — Misescapes, Madrid — Made with 💜 by Misescapes
+      </div>
+    </v-footer>
+</template>
